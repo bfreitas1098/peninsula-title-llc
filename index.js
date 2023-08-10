c1bf1eb4f82a8969a123d32f1555f44b4b3174e1
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2023-08-10 13:00:01 -0400

    Made changes to the home page and transaction coordinator services pages UI

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 /* Dropdown menu */
 
 const dropDown = document.getElementById("dropdown-menu");
+const downArrow = document.getElementById("down-arrow");
+const upArrow = document.getElementById("up-arrow");
 
-function makeVisible() {
+const makeVisible = () => {
   dropDown.classList.toggle("visible");
+  downArrow.classList.toggle("not-visible");
+  upArrow.classList.toggle("visible");
+};
+
+/* FAQ */
+
+const accordion = document.getElementsByClassName("questions-box");
+
+for (let i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    let answer = this.nextElementSibling;
+
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+    } else {
+      answer.style.display = "block";
+    }
+  });
 }
