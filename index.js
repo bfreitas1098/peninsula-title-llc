f247e463d515aafafa27837c1cc7d6bb41660752
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2024-01-11 12:05:39 -0500

    Refactored the code for the FAQ section on the home page and added comments to JS code

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const dropDown = document.getElementById("dropdown-menu");
 const downArrow = document.getElementById("down-arrow");
 const upArrow = document.getElementById("up-arrow");
 
+// open and close dropdown menu
 const makeDDVisible = () => {
   dropDown.classList.toggle("visible");
   downArrow.classList.toggle("not-visible");
@@ -14,6 +15,7 @@ const rAboutMenu = document.getElementById("r-about");
 const downArr = document.getElementById("down-arr");
 const upArr = document.getElementById("up-arr");
 
+// open and close about menu in dropdown
 const makeRMVisible = () => {
   rAboutMenu.classList.toggle("visible");
   downArr.classList.toggle("not-visible");
@@ -22,35 +24,35 @@ const makeRMVisible = () => {
 
 const responsiveMenu = document.getElementById("responsive-menu-container");
 
+// open and close responsive menu
+const openRMenu = () => {
+  responsiveMenu.classList.toggle("visible");
+};
 const closeRMenu = () => {
   responsiveMenu.classList.toggle("visible");
 };
 
-const openRMenu = () => {
-  responsiveMenu.classList.toggle("visible");
-};
-
 /* FAQ */
+const accordion = document.querySelectorAll(".questions-box");
+const answers = document.querySelectorAll(".answer");
 
-const accordion = document.getElementsByClassName("questions-box");
-
+// Display the answers to questions as the questions are clicked then hide them again
 for (let i = 0; i < accordion.length; i++) {
-  accordion[i].addEventListener("click", function () {
-    let answer = this.nextElementSibling;
-
-    if (answer.style.display === "block") {
-      answer.style.display = "none";
-    } else {
-      answer.style.display = "block";
-    }
-  });
+  for (let i = 0; i < answers.length; i++) {
+    accordion[i].addEventListener("click", () => {
+      const isAnswerOpen = answers[i].classList.contains("open");
+      answers[i].classList.remove("open");
+      if (!isAnswerOpen) {
+        answers[i].classList.toggle("open");
+      }
+    });
+  }
 }
 
 /* Footer year */
 
-const present = new Date();
-const currentYear = present.getFullYear();
-
+// Always display the current year in the footer
+const currentYear = new Date().getFullYear();
 document.getElementById("year").textContent = currentYear;
 
 /* Testimonials */
@@ -60,6 +62,7 @@ const authorNames = document.querySelectorAll(".testimonial-author");
 const initialsArr = Array.from(initials);
 
 let authorsArr = [];
+// Take the authors names and convert them into initials
 authorNames.forEach((author) => {
   let aut = author.innerHTML;
   authorsArr.push(
@@ -71,6 +74,7 @@ authorNames.forEach((author) => {
   );
 });
 
+// Replace the text of the initials elements with the intitals created from the author names
 for (let i = 0; i < initialsArr.length; i++) {
   for (let i = 0; i < authorsArr.length; i++) {
     initialsArr[i].innerHTML = authorsArr[i];
